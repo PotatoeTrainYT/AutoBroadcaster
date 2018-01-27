f4c10f5416974a744e8f426e2cfe12d2522c87c7
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2018-01-27 14:25:34 +0000

    Create Main.php

---
+++ src/AutoBroadcaster/Main.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AutoBroadcaster;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\utils\Config;
+
+class Main extends PluginBase {
+
+    public function onEnable() : void {
+        $this->getLogger()->info("AutoBroadcaster Enabled! Plugin by Potatoe. Download at https://github.com/PotatoeTrainYT/AutoBroadcaster/");
+        @mkdir($this->getDataFolder());
+        $this->saveResource("settings.yml");
+        $this->settings = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
+        $this->scheduler();
+    }
+
+    public function scheduler() {
+        if(is_numeric($this->settings->get("seconds"))) {
+            $this->getServer()->getScheduler()->scheduleRepeatingTask(new BroadcastTask($this), $this->settings->get("seconds") * 20);
+        } else {
+            $this->getLogger()->warning("Plugin disabling, Seconds is not a numeric value please edit");
+            $this->getPluginLoader()->disablePlugin($this);
+        }
+    }
+}
